ecdeb3371ae391ca5fc273ac755d2a43e47c6c81
minco <jeonghwan.seo@mincoding.co.kr>
2024-05-17 14:05:34 +0900

    second commit

--- baseball_test/test.cpp
+++ baseball_test/test.cpp
@@ -1,6 +1,31 @@
 #include "pch.h"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+#include "../baseball-3/baseball.cpp"
+
+class BaseballFixture : public testing::Test {
+public:
+	Baseball game{ "123" };
+	void assertIllegalArgument(string guessNumber) {
+		try {
+			game.guess(guessNumber);
+			FAIL();
+		}
+		catch (exception e) {
+			//PASS
+		}
+	}
+};
+
+TEST_F(BaseballFixture, ThrowExceptionWhenInvalidCase) {
+	assertIllegalArgument("12");
+	assertIllegalArgument("12s");
+	assertIllegalArgument("121");
+}
+
+TEST_F(BaseballFixture, ReturnSolvedResultIfMatchedNumber) {
+	GuessResult result = game.guess("123");
+
+	EXPECT_TRUE(result.solved);
+	EXPECT_EQ(3, result.strikes);
+	EXPECT_EQ(0, result.balls);
 }
